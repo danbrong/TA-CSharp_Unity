4782ddbea84de95cf17f6f3d3a20ff3906c7c8c4
danbrong <bchang91@gmail.com>
2023-04-26 11:08:29 -0700

    Created Daily Report Assignment

---
+++ Basic_C#_Programs/DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number are you on?");
+            string pageNum = Console.ReadLine();
+            int myPage = Convert.ToInt32(pageNum);
+
+            Console.WriteLine("Do you need help with anything else? Please answer True or False");
+            string myHelp = Console.ReadLine();
+            bool HelpMe = bool.Parse(myHelp);
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Be Specific.");
+            string myFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string myStudy = Console.ReadLine();
+            int studyHours = Convert.ToInt32(myStudy);
+
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+
+
+            //// Answer Checking
+            //Console.WriteLine(yourName);
+            //Console.WriteLine(yourCourse);
+            //Console.WriteLine(myPage);
+            //Console.WriteLine(HelpMe);
+            //Console.WriteLine(myFeedback);
+            //Console.WriteLine(studyHours);
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VariablesAndDataType/VariablesAndDataType/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is how to comment code for a single line.
+            //Console.WriteLine("What is your name? ");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favNum = Console.ReadLine();
+            //int favNumber = Convert.ToInt32(favNum);
+            //int total = favNumber + 5;
+            //Console.WriteLine("Your favorite number is: " + total);
+            //Console.ReadLine();
+
+
+            //bool isStuyding = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char questionMark = '\u2103';
+
+
+            //decimal moneyBank = 100.5m;
+            //double heightInCm = 2111.30202091;
+            //float secondLeft = 2.62f;
+            //short tempMars = -341;
+
+
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
+            ////Console.ReadLine(yearsOld);
+
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+        }
+    }
+}
