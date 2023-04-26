15d5a6f72423a1b9633a77607e9ce652c6d7e400
danbrong <bchang91@gmail.com>
2023-04-26 11:55:29 -0700

    Created Anonymous Income Assignment

---
+++ Basic_C#_Programs/AnonymousIncomeAssignment/AnonymousIncomeAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousIncomeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+
+            Console.WriteLine("Person 1:");
+
+            Console.WriteLine("What is your Hourly Rate?");
+            string myRate = Console.ReadLine();
+            float Rate = float.Parse(myRate);
+
+            Console.WriteLine("How many hours do you work a week?");
+            string myHours = Console.ReadLine();
+            int Hours = Convert.ToInt32(myHours);
+
+
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("What is your Hourly Rate?");
+            string myRate2 = Console.ReadLine();
+            float Rate2 = float.Parse(myRate2);
+
+            Console.WriteLine("How many hours do you work a week?");
+            string myHours2 = Console.ReadLine();
+            int Hours2 = Convert.ToInt32(myHours2);
+
+
+            Console.WriteLine("Person 1: Annual Salary");
+            float Salary = Rate * Hours * 52;
+            Console.WriteLine(Salary);
+
+            Console.WriteLine("Person 2: Annual Salary");
+            float Salary2 = Rate2 * Hours2 * 52;
+            Console.WriteLine(Salary2);
+
+
+            Console.WriteLine("Does Person 1 make more money than person 2?");
+            bool myComparison = Salary > Salary2;
+            Console.WriteLine(myComparison);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number, it will be multiplied by 50.");
+            string myNum = Console.ReadLine();
+            double Num = Convert.ToDouble(myNum);
+            double product = Num * 50;
+            Console.WriteLine(product);
+
+            Console.WriteLine("Pick another number, we will add 25 to it.");
+            string myNum2 = Console.ReadLine();
+            int Num2 = Convert.ToInt32(myNum2);
+            int sum = Num2 + 25;
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Pick another number, we will divide it by 12.5.");
+            string myNum3 = Console.ReadLine();
+            double Num3 = Convert.ToDouble(myNum3);
+            double quotient = Num3 / 12.5;
+            Console.WriteLine(quotient);
+
+            Console.WriteLine("Pick a number, we will check if it is greater than 50.");
+            string myNum4 = Console.ReadLine();
+            int Num4 = Convert.ToInt32(myNum4);
+            bool greaterThan = Num4 > 50;
+            Console.WriteLine(greaterThan);
+
+            Console.WriteLine("Pick a number, we will divide by 7 and find the remainder.");
+            string myNum5 = Console.ReadLine();
+            double Num5 = Convert.ToDouble(myNum5);
+            double remainder = Num5 % 7;
+            Console.WriteLine(remainder);
+
+
+            Console.ReadLine();
+        }
+    }
+}
