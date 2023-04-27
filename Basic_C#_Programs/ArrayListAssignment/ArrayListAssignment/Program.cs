8625d3e5dfe856c5f9b333c7d43276a2feb4a264
danbrong <bchang91@gmail.com>
2023-04-27 16:21:19 -0700

    Created Array List Assigment

---
+++ Basic_C#_Programs/ArrayListAssignment/ArrayListAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sopranos = { "Tony", "Carmella", "Meadow", "Anthony Jr.", "Junior", };
+            Console.WriteLine("Choose an index position between 0 and 4.");
+            int choice1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (choice1 >= 0 && choice1 <= 4)
+            {
+                Console.WriteLine("You've chosen: " + Sopranos[choice1]);
+                //Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You have chosen an incorrect index, we'll move on.");
+            }
+
+            int[] Prime = { 1, 2, 3, 5, 7, 11, 13 };
+            Console.WriteLine("Choose an index position between 0 and 6.");
+            int choice2 = Convert.ToInt32(Console.ReadLine());
+
+            if (choice2 >= 0 && choice2 <= 6)
+            {
+                Console.WriteLine("You've chosen: " + Prime[choice2]);
+                //Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You have chosen an incorrect index, we'll move on.");
+            }
+
+            List<string> stringList = new List<string>();
+            stringList.Add("Index 0");
+            stringList.Add("Index 1");
+            stringList.Add("Index 2");
+            stringList.Add("Index 3");
+            stringList.Add("Index 4");
+
+            Console.WriteLine("Choose an index position between 0 and 4.");
+            int choice3 = Convert.ToInt32(Console.ReadLine());
+
+            if (choice3 >= 0 && choice3 <= 4)
+            {
+                Console.WriteLine("You've chosen: " + stringList[choice3]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You have chosen an incorrect index, we're closing this program next time you press \"Enter\"");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringPractice/StringPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string part1 = "This is the first part, ";
+            //string part2 = "this is the second part, ";
+            //string part3 = "and this is the third part.";
+
+            //string full = part1 + part2 + part3;
+            //Console.WriteLine(full);
+            //Console.ReadLine();
+
+
+            //string lower = "this should be uppercase.";
+            //string upper = lower.ToUpper();
+            //Console.WriteLine(upper);
+            //Console.ReadLine();
+
+         
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("My Name is Brandon");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
