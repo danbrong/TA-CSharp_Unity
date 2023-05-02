2f710c18381c608e752bcd2e5985101cb3ae1d5f
danbrong <bchang91@gmail.com>
2023-05-02 15:30:12 -0700

    Created Method Submission Assignment

---
+++ Basic_C#_Programs/MethodSubmissionAssignment/MethodSubmissionAssignment/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    public class Methods
+    {
+        public static int Optional(int x, int y = 14)
+        {
+            int sum = x + y;
+            return sum;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods optional = new Methods();
+
+
+            Console.WriteLine("Enter your first number.");
+            string stringX = (Console.ReadLine());
+            int x = Convert.ToInt32(stringX);
+
+            Console.WriteLine("Enter a second number, or just press Enter.");
+            string stringY = (Console.ReadLine());
+            int y = Convert.ToInt32(stringY);
+
+            Console.WriteLine(Methods.Optional(x, y));
+            Console.ReadLine();
+
+        }
+    }
+}
