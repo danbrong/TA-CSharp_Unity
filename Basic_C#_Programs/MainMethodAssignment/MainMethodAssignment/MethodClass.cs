588d62e8c643501306beca803b6ef494eb6bcea7
danbrong <bchang91@gmail.com>
2023-05-02 15:19:47 -0700

    Created Main Method Assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/MethodClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class MethodClass
+    {
+        public static int MyOp(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        public static int MyDec(double x, double y)
+        {
+            int diff = Convert.ToInt32(x - y);
+            return diff;
+        }
+
+        public static int MyConvert(string x, string y)
+        {
+            int conX = Convert.ToInt32(x);
+            int conY = Convert.ToInt32(y);
+            int convert = conX + conY;
+            return convert;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodClass addition = new MethodClass();
+            MethodClass difference = new MethodClass();
+
+            Console.WriteLine(MethodClass.MyOp(2, 2));
+
+            Console.WriteLine(MethodClass.MyDec(3.14, 2.12));
+
+            Console.WriteLine(MethodClass.MyConvert("3", "4"));
+            Console.ReadLine();
+        }
+    }
+}
