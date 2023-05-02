1fdd12993f653fdbc995c28db1e51ebfde195f69
danbrong <bchang91@gmail.com>
2023-05-02 15:04:39 -0700

    Fixed Method Calling Assignment

--- Basic_C#_Programs/MethodPractice/MethodPractice/MathMethods.cs
+++ Basic_C#_Programs/MethodPractice/MethodPractice/MathMethods.cs
@@ -6,23 +6,24 @@ using System.Threading.Tasks;
 
 namespace MethodPractice
 {
-    public static void myAdd(int x, int y)
+    public class MathMethods
     {
-        int sum = x + y;
-        Console.WriteLine(sum);
-    }
+        public static int MyAdd(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
 
-    public static int MySub(int x, int y)
-    {
-        int diff = x - y;
-        return;
-    }
+        public static int MySub(int x, int y)
+        {
+            int diff = x - y;
+            return diff;
+        }
 
-    public static int MyMult(int x, int y)
-    {
-        int prod = x * y;
-        return;
+        public static int MyMult(int x, int y)
+        {
+            int prod = x * y;
+            return prod;
+        }
     }
-    
-    
 }
--- Basic_C#_Programs/MethodPractice/MethodPractice/Program.cs
+++ Basic_C#_Programs/MethodPractice/MethodPractice/Program.cs
@@ -10,7 +10,24 @@ namespace MethodPractice
     {
         static void Main()
         {
+            MathMethods addMethod = new MathMethods();
+            MathMethods subMethod = new MathMethods();
+            MathMethods multMethod = new MathMethods();
+
+           
+
+            Console.WriteLine("Pick a number to do math operations with.");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number to do math operations with.");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(firstNum + " plus " + secondNum + " equals " + MathMethods.MyAdd(firstNum, secondNum));
+            Console.WriteLine(firstNum + " minus " + secondNum + " equals " + MathMethods.MySub(firstNum, secondNum));
+            Console.WriteLine(firstNum + " multiplied by " + secondNum + " equals " + MathMethods.MyMult(firstNum, secondNum));
             
+
+            Console.ReadLine();
         }
+        
     }
 }
