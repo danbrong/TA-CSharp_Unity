4dc4e1fba1b8b914efb94a24ee3b6252a8445940
danbrong <bchang91@gmail.com>
2023-05-02 16:39:17 -0700

    Created ClassSubmissionAssignment

---
+++ Basic_C#_Programs/ClassSubmissionAssignment/ClassSubmissionAssignment/VoidMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class VoidMethod
+    {
+        public void MyDivide(int x)
+        {
+            int quotient = x / 2;
+            Console.WriteLine(quotient);
+        }
+
+        public void MyDivide(int y, int z)
+        {
+            int quotient2 = (y + z)/2;
+            Console.WriteLine(quotient2);
+        }
+        public void MyOutput(out int x)
+        {
+            int output = 3;
+            x = output;
+            Console.WriteLine(output);
+        }
+
+        public static void MyStatic()
+        {
+            string Static = "Static Statement";
+            Console.WriteLine(Static);
+        }
+    }
+}
