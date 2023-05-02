3bfcad4fc23252c3034f694977b9d447ba78ec25
danbrong <bchang91@gmail.com>
2023-05-02 15:06:22 -0700

    Slight Update

--- Basic_C#_Programs/MethodPractice/MethodPractice/Program.cs
+++ Basic_C#_Programs/MethodPractice/MethodPractice/Program.cs
@@ -10,11 +10,13 @@ namespace MethodPractice
     {
         static void Main()
         {
-            MathMethods addMethod = new MathMethods();
-            MathMethods subMethod = new MathMethods();
-            MathMethods multMethod = new MathMethods();
+            //MathMethods addMethod = new MathMethods();
+            //MathMethods subMethod = new MathMethods();
+            //MathMethods multMethod = new MathMethods();
 
-           
+            // If an instructor is reading this, when would I need the above code, and when can I skip it?
+
+
 
             Console.WriteLine("Pick a number to do math operations with.");
             int firstNum = Convert.ToInt32(Console.ReadLine());
