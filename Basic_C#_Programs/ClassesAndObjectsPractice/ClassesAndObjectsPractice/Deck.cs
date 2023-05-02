e4b5fa62032115d89f8d86c82c42f1886781f544
danbrong <bchang91@gmail.com>
2023-05-02 16:04:38 -0700

    Moved Deck Shuffle to Deck Method

--- Basic_C#_Programs/ClassesAndObjectsPractice/ClassesAndObjectsPractice/Deck.cs
+++ Basic_C#_Programs/ClassesAndObjectsPractice/ClassesAndObjectsPractice/Deck.cs
@@ -9,6 +9,7 @@ namespace ClassesAndObjectsPractice
     public class Deck
     {
         public Deck()
+
         {
             Cards = new List<Card>();
             List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
@@ -29,5 +30,25 @@ namespace ClassesAndObjectsPractice
             }
         }
         public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
+
+    
+
     }
 }
