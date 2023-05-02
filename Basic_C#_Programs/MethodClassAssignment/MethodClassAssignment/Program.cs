eb74c6a121a8e4d94fc1b4c5dec800b0f1cd0a4a
danbrong <bchang91@gmail.com>
2023-05-02 15:49:48 -0700

    Method Class Assignment

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Void_Method myObj = new Void_Method();
+
+            myObj.MyOp(2, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Void Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Void_Method
+    {
+        public void MyOp(int x, int y);
+        int sum = x + 2;
+        return y;
+
+    }
+}
