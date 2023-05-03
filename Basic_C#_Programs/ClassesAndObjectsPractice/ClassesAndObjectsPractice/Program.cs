49c2630c77dae4b6898cee62095a28c9981c4227
danbrong <bchang91@gmail.com>
2023-05-02 17:20:03 -0700

    Created Method and Objects Assignment

--- Basic_C#_Programs/ClassesAndObjectsPractice/ClassesAndObjectsPractice/Program.cs
+++ Basic_C#_Programs/ClassesAndObjectsPractice/ClassesAndObjectsPractice/Program.cs
@@ -10,6 +10,12 @@ namespace ClassesAndObjectsPractice
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Brandon", "Bill", "Joe" };
+            game.ListPlayers();
+            game.Play();
+            Console.ReadLine();
+
             Deck deck = new Deck();
             deck.Shuffle(3);
 
