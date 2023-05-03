f7c7ff935fea790e29767c74d4f9d0419250e624
danbrong <bchang91@gmail.com>
2023-05-03 13:58:27 -0700

    EnumAssignment Created

---
+++ Basic_C#_Programs/EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        public enum Days
+            {
+                Monday,
+                Tuesday,
+                Wednesday,
+                Thursday,
+                Friday,
+                Saturday,
+                Sunday
+            }
+        static void Main()
+        {
+
+            try
+            {
+                Console.WriteLine("Please enter a day of the week.");
+                string choice = Console.ReadLine();
+                Days dayChoice = (Days) Enum.Parse(typeof(Days));
+                Console.WriteLine(dayChoice);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid day of the week.");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
