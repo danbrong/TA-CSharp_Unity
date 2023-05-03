669451c23639866b12fc66fc3ec44426bd838b8e
danbrong <bchang91@gmail.com>
2023-05-03 15:32:46 -0700

    Lambda Assignment Created

---
+++ Basic_C#_Programs/LambdaAssignment/LambdaAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> Employees = new List<Employee>()
+            {
+               new Employee() {Id = 1, firstName = "Joe", lastName = "Plumber" },
+               new Employee() {Id = 2, firstName = "Joe", lastName = "Richardson" },
+               new Employee() {Id = 3, firstName = "Brandon", lastName = "Chang" },
+               new Employee() {Id = 4, firstName = "Lisa", lastName = "Tarbuck" },
+               new Employee() {Id = 5, firstName = "Gene", lastName = "Simmons" },
+               new Employee() {Id = 6, firstName = "Tiffany", lastName = "Pancakes" },
+               new Employee() {Id = 7, firstName = "Bill", lastName = "Williamson" },
+               new Employee() {Id = 8, firstName = "Jane", lastName = "Doe" },
+               new Employee() {Id = 9, firstName = "Bob", lastName = "Opedic" },
+               new Employee() {Id = 10, firstName = "Nicki", lastName = "Minaj" },
+            };
+
+
+            // For Each Joe List
+            foreach (Employee employee in Employees)
+            {
+
+                if (employee.firstName == "Joe")
+                {
+                    List<Employee> joePloyee = new List<Employee>();
+                    joePloyee.Add(employee);
+                }
+                Console.WriteLine(employee.firstName + " " + employee.lastName + ", " + employee.Id);
+            }
+
+            // Lambda Joe List
+            List<Employee> joePloyee2 = Employees.Where(j => j.firstName == "Joe").ToList();
+
+            Console.WriteLine(joePloyee2.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
